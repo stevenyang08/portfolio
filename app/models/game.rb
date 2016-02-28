de375a397790ddc7c91b407ade007ee7cf13b21c
kgadams93 <kgadams93@gmail.com>
2016-02-28 19:44:29 +0000

    Update game title validation to max length 30 char

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ActiveRecord::Base
   
-  validates :title, presence: true, length: { minimum: 2, maximum: 20 }
+  validates :title, presence: true, length: { minimum: 2, maximum: 30 }
   validates :description, presence: true, length: { minimum: 2, maximum: 120 }
   validates :live_link, length: { minimum: 5, maximum: 100 }
   validates :image, presence: true
